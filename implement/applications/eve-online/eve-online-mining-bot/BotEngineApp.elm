cfe2ae0266d08245e115f3fd1678bf5b46e9b36b
ccvfrenzy <84857026+ccvfrenzy@users.noreply.github.com>
2021-06-04 21:06:41 -0300

    Update BotEngineApp.elm

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -91,7 +91,7 @@ import Regex
 -}
 defaultBotSettings : BotSettings
 defaultBotSettings =
-    { runAwayShieldHitpointsThresholdPercent = 30
+    { runAwayShieldHitpointsThresholdPercent = 50
     , unloadStationName = Nothing
     , unloadStructureName = Nothing
     , modulesToActivateAlways = []
